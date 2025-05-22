cd637a6bef6954647219eec5f92847a9fea9d134
Gustavo Ferreira Dos Santos <gustavofrsnt@gmail.com>
2025-05-22 17:38:25 -0300

    Add validation

--- src/routes.js
+++ src/routes.js
@@ -30,6 +30,43 @@ const hasSaldo = async (user_id, transfer_value, client_connection) => {
     return true
 }
 
+const authorizeTransfer = async (payer, payee, value) => {
+    try {
+        const response = await fetch(process.env.AUTH_URL)
+        if (response.ok) {
+            return false
+        }
+        return true
+    } catch (e) {
+        // -- MELHORAR O TRATAMENTO DE ERROS AQUI --
+        return false
+    }
+}
+
+const makeTransfer = async (payer, payee, value, client_connection) => {
+    try{
+        console.log(`                        
+UPDATE transfers."carteira" c
+SET saldo = saldo - ${value}
+FROM transfers."usuario" u
+WHERE c.id = u.carteira
+  AND u.id = ${payer};
+        `)
+        await client_connection.query(`
+                        UPDATE transfers."carteira" c
+SET saldo = saldo - $1
+FROM transfers."usuario" u
+WHERE c.id = u.carteira
+  AND u.id = $2;
+        `, [value, payer])
+
+        return true
+    } catch (e) {
+        console.log(e)
+        return false
+    }
+}
+
 router.route("/transfer")
     .post(async (req, res) => {
         const transfer = req.body
@@ -42,6 +79,12 @@ router.route("/transfer")
         if (!(await hasSaldo(transfer.payer, transfer.value, client_connection))) {
             return res.status(400).json({"success": false, "message": "Saldo insuficiente!"}) 
         }
+
+        if (!(await authorizeTransfer(transfer.payer, transfer.payee, transfer.value))) {
+            return res.status(400).json({"success": false, "message": "Transferência não autorizada!"})
+        }
+
+        makeTransfer(transfer.payer, transfer.payee, transfer.value)
 
         // -- TERMINAR A LÓGICA DA TRANSFERÊNCIA --
 
